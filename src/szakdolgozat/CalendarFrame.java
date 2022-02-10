85e9118c6e6ffaf6c190e2beddb3f1d115d906d0
ATiKA0 <gluckati78@gmail.com>
2022-02-10 23:04:26 +0100

    2.1
    Parsed the ics file succesfully

--- src/szakdolgozat/CalendarFrame.java
+++ src/szakdolgozat/CalendarFrame.java
@@ -5,9 +5,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
+import org.apache.commons.io.FileUtils;
+
 import com.toedter.calendar.IDateEvaluator;
 import com.toedter.calendar.JCalendar;
 import com.toedter.calendar.JDateChooser;
+
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.data.ParserException;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -16,7 +21,21 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Calendar;
+import java.beans.PropertyChangeListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.beans.PropertyChangeEvent;
 
 public class CalendarFrame {
 
@@ -80,12 +99,41 @@ public class CalendarFrame {
         evaluator.add(createDate(2022,01,14));
         evaluator.add(createDate(2022,00,15));
         JCalendar jc = new JCalendar();
+        jc.addPropertyChangeListener("calendar", new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent e) {
+            	try {
+					parse();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (ParserException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+            }
+        });
         jc.getDayChooser().addDateEvaluator(evaluator);
         jc.setCalendar(jc.getCalendar());
         f.getContentPane().add(jc);
         f.pack();
         f.setLocationRelativeTo(null);
         f.setVisible(true);
+    }
+    
+    public String readFile(String string, Charset charset) throws IOException {
+        File file = new File(string);
+        return FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+    }
+    
+    public void parse() throws IOException, ParserException {
+    	String content = readFile("C:\\Users\\gluck\\Downloads\\igen.ics", Charset.defaultCharset());
+    	System.setProperty("ical4j.unfolding.relaxed", "true");
+    	System.setProperty("ical4j.parsing.relaxed", "true");
+    	StringReader sin = new StringReader(content);
+    	CalendarBuilder builder = new CalendarBuilder();
+    	net.fortuna.ical4j.model.Calendar calendar = builder.build(sin);
+    	JOptionPane.showMessageDialog(null, calendar.getProperties());
     }
 
     private Date createDate(int y, int m, int d) {
