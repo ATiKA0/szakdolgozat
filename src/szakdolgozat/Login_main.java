86ff8af80a1e99cd4bfc49758adbe1e191983bfc
ATiKA0 <gluckati78@gmail.com>
2023-09-21 20:55:29 +0200

    Registration/login fully working

--- src/szakdolgozat/Login_main.java
+++ src/szakdolgozat/Login_main.java
@@ -6,9 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -20,14 +18,14 @@ import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-import org.bouncycastle.crypto.generators.BCrypt;
-
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
+import java.awt.Toolkit;
 
 public class Login_main {
 
-	private JFrame frmBejelentkezs;
+	protected static String user;
+	protected JFrame frmLogin;
 	private static JTextField t_usrn;
 	private JPasswordField t_passwd;
 	/**
@@ -38,7 +36,7 @@ public class Login_main {
 			public void run() {
 				try {
 					Login_main window = new Login_main();
-					window.frmBejelentkezs.setVisible(true);
+					window.frmLogin.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -58,75 +56,81 @@ public class Login_main {
 	 */
 	private void initialize() {
 		System.setProperty("file.encoding","UTF-8");
-		frmBejelentkezs = new JFrame();
-		frmBejelentkezs.setTitle("Bejelentkezés");
-		frmBejelentkezs.setBounds(100, 100, 490, 334);
-		frmBejelentkezs.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmLogin = new JFrame();
+		frmLogin.setIconImage(Toolkit.getDefaultToolkit().getImage(Login_main.class.getResource("/szakdolgozat/calendar.png")));
+		frmLogin.setTitle("Bejelentkezés");
+		frmLogin.setBounds(100, 100, 490, 334);
+		frmLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JButton b_login = new JButton("Bejelentkezés");
-		b_login.setBounds(68, 194, 135, 32);
+		b_login.setBounds(73, 218, 135, 32);
 		b_login.addActionListener(new ActionListener() {	//Login button
 			public void actionPerformed(ActionEvent e) {
-				if(isExist(t_usrn.getText(),t_passwd.getText())) {
-					frmBejelentkezs.dispose();
+				if(isValid(t_usrn.getText(),t_passwd.getText())) {
+					user = t_usrn.getText();
+					frmLogin.dispose();
 					CalendarFrame.main(null);
 				}
 				else {
-					JOptionPane.showMessageDialog(null, "Nincs ilyen felhasználó!\n"+"Kérem regisztráljon!");
+					t_usrn.setText(null);
+					t_passwd.setText(null);
+					JOptionPane.showMessageDialog(null, "Hibás bejelentkezési adatok!");
 				}
 			}
 		});
-		frmBejelentkezs.getContentPane().setLayout(null);
-		frmBejelentkezs.getContentPane().add(b_login);
+		frmLogin.getContentPane().setLayout(null);
+		frmLogin.getContentPane().add(b_login);
 		
 		t_usrn = new JTextField();
 		t_usrn.setBounds(118, 62, 238, 26);
-		frmBejelentkezs.getContentPane().add(t_usrn);
+		frmLogin.getContentPane().add(t_usrn);
 		t_usrn.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Jelsz\u00F3");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		lblNewLabel.setBounds(213, 106, 48, 26);
-		frmBejelentkezs.getContentPane().add(lblNewLabel);
+		frmLogin.getContentPane().add(lblNewLabel);
 		
 		JLabel lblFelhasznlnv = new JLabel("Felhaszn\u00E1l\u00F3n\u00E9v");
 		lblFelhasznlnv.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		lblFelhasznlnv.setBounds(179, 18, 117, 26);
-		frmBejelentkezs.getContentPane().add(lblFelhasznlnv);
+		frmLogin.getContentPane().add(lblFelhasznlnv);
 		
 		t_passwd = new JPasswordField();
 		t_passwd.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {	//This do the same what the button do but with an "ENTER" button hit
 				 if (e.getKeyCode()==KeyEvent.VK_ENTER){
-					 if(isExist(t_usrn.getText(),t_passwd.getText())) {
-						 frmBejelentkezs.dispose();
+					 if(isValid(t_usrn.getText(),t_passwd.getText())) {
+						 user = t_usrn.getText();
+						 frmLogin.dispose();
 						 CalendarFrame.main(null);
 					 }
 					 else {
-						 JOptionPane.showMessageDialog(null, "Nincs ilyen felhasználó!\n"+"Kérem regisztráljon!");
+						 t_usrn.setText(null);
+						 t_passwd.setText(null);
+						 JOptionPane.showMessageDialog(null, "Hibás bejelentkezési adatok!");
 					 }
 				 }
 				 }
 		});
 		t_passwd.setEchoChar('*');
 		t_passwd.setBounds(118, 150, 238, 26);
-		frmBejelentkezs.getContentPane().add(t_passwd);
+		frmLogin.getContentPane().add(t_passwd);
 		
-		JButton b_registration = new JButton("Regisztráció");
+		JButton b_registration = new JButton("Regisztráció >>");
 		b_registration.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {		//Registration button
-				registration(t_usrn.getText(),t_passwd.getText());
-				frmBejelentkezs.dispose();
-				CalendarFrame.main(null);
+				frmLogin.setVisible(false);
+				Register_main.main(null);
 			}
 		});
-		b_registration.setBounds(271, 194, 135, 32);
-		frmBejelentkezs.getContentPane().add(b_registration);
+		b_registration.setBounds(276, 218, 135, 32);
+		frmLogin.getContentPane().add(b_registration);
 	}
 	
-	public Boolean isExist(String name, String passwd) {
+	private Boolean isValid(String name, String passwd) {
 		
-		Boolean exist = false;
+		Boolean valid = false;
 		Argon2 argon2 = Argon2Factory.create();
 		try {
 			Connection connection = Func.connectToSql();
@@ -135,40 +139,17 @@ public class Login_main {
 			result.next();
 			if(result.getRow() == 1) {
 				String pass = result.getString(3);
-				exist = argon2.verify(pass, passwd);
+				valid = argon2.verify(pass, passwd);
 			}
 			statement.close();
 			connection.close();
 		} catch (SQLException e) {
 			throw new IllegalStateException("Cannot connect the database!", e);
 		}
-		return exist;
+		return valid;
 	}
 	
-	private void registration(String name, String passwd) {
-		
-		try {
-			if(isExist(name,passwd)==true) {
-				JOptionPane.showMessageDialog(null, "Ez a felhasználó már létezik, most be lesz léptetve!");
-			}
-			else {
-				String email = "";
-				Argon2 argon2 = Argon2Factory.create();
-				String hashedPassword = argon2.hash(10, 65536, 1, passwd);
-				Connection connection = Func.connectToSql();
-				String procedureCall = "{call InsertUserWithTable(?, ?, ?)}";
-	    		CallableStatement callableStatement = connection.prepareCall(procedureCall);
-	    		callableStatement.setString(1, name);
-	    		callableStatement.setString(2, email);
-	    		callableStatement.setString(3, hashedPassword);
-	    		callableStatement.execute();
-				connection.close();	
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
 	public static String getUsrn() {
-		return t_usrn.getText();
+		return user;
 	}
 }
--- src/szakdolgozat/Register_main.java
+++ src/szakdolgozat/Register_main.java
@@ -6,6 +6,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,12 +19,16 @@ import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+import java.awt.Toolkit;
+
 public class Register_main extends Login_main{
 
 	private JFrame frmRegistration;
 	private static JTextField t_usrn;
 	private JPasswordField t_passwd;
-	private JPasswordField passwordField;
+	private JTextField t_email;
 
 	/**
 	 * Launch the application.
@@ -50,7 +59,8 @@ public class Register_main extends Login_main{
 	private void initialize() {
 		System.setProperty("file.encoding","UTF-8");
 		frmRegistration = new JFrame();
-		frmRegistration.setTitle("Bejelentkezés");
+		frmRegistration.setIconImage(Toolkit.getDefaultToolkit().getImage(Register_main.class.getResource("/szakdolgozat/calendar.png")));
+		frmRegistration.setTitle("Regisztráció");
 		frmRegistration.setBounds(100, 100, 490, 334);
 		frmRegistration.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmRegistration.getContentPane().setLayout(null);
@@ -75,15 +85,14 @@ public class Register_main extends Login_main{
 			@Override
 			public void keyPressed(KeyEvent e) {	//This do the same what the button do but with an "ENTER" button hit
 				 if (e.getKeyCode()==KeyEvent.VK_ENTER){
-					 if(isExist(t_usrn.getText(),t_passwd.getText())) {
-						 frmRegistration.dispose();
-						 CalendarFrame.main(null);
-					 }
-					 else {
-						 JOptionPane.showMessageDialog(null, "Nincs ilyen felhasználó!\n"+"Kérem regisztráljon!");
-					 }
-				 }
-				 }
+					if(registration(t_usrn.getText(), t_email.getText(), t_passwd.getText())) {
+						Login_main.user = t_usrn.getText();
+						frmRegistration.dispose();
+						frmLogin.dispose();
+						CalendarFrame.main(null);					
+					}
+			 	}
+			}
 		});
 		t_passwd.setEchoChar('*');
 		t_passwd.setBounds(119, 208, 238, 26);
@@ -92,12 +101,15 @@ public class Register_main extends Login_main{
 		JButton b_registration = new JButton("Regisztráció");
 		b_registration.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {		//Registration button
-				registration(t_usrn.getText(),t_passwd.getText());
-				frmRegistration.dispose();
-				CalendarFrame.main(null);
+				if(registration(t_usrn.getText(), t_email.getText(), t_passwd.getText())) {
+					Login_main.user = t_usrn.getText();
+					frmRegistration.dispose();
+					frmLogin.dispose();
+					CalendarFrame.main(null);					
+				}
 			}
 		});
-		b_registration.setBounds(170, 247, 135, 32);
+		b_registration.setBounds(68, 244, 135, 32);
 		frmRegistration.getContentPane().add(b_registration);
 		
 		JLabel lblEmail = new JLabel("Email");
@@ -105,9 +117,78 @@ public class Register_main extends Login_main{
 		lblEmail.setBounds(214, 91, 48, 26);
 		frmRegistration.getContentPane().add(lblEmail);
 		
-		passwordField = new JPasswordField();
-		passwordField.setEchoChar('*');
-		passwordField.setBounds(119, 130, 238, 26);
-		frmRegistration.getContentPane().add(passwordField);
+		t_email = new JTextField();
+		t_email.setBounds(119, 130, 238, 26);
+		frmRegistration.getContentPane().add(t_email);
+		
+		JButton b_login = new JButton("Bejelentkezés >>");
+		b_login.addActionListener(new ActionListener() { //Back to the login window
+			public void actionPerformed(ActionEvent e) {
+				frmRegistration.dispose();
+				frmLogin.setVisible(true);
+			}
+		});
+		b_login.setBounds(271, 244, 135, 32);
+		frmRegistration.getContentPane().add(b_login);
 	}
+	
+	/**
+	 * Uploads the user data to the database if not exist with password encryption
+	 * @param name : User name
+	 * @param email : User email
+	 * @param passwd : User password
+	 * @return True if the registration is successful
+	 */
+	private Boolean registration(String name, String email, String passwd) {
+			Boolean success = true;
+			try {
+				if(isExist(name,email)==true) {
+					JOptionPane.showMessageDialog(null, "Ez az email cím/felhasználónév már létezik!");
+					t_usrn.setText(null);
+					t_email.setText(null);
+					t_passwd.setText(null);
+					success = false;
+				}
+				else {
+					Argon2 argon2 = Argon2Factory.create();
+					String hashedPassword = argon2.hash(10, 65536, 1, passwd);
+					Connection connection = Func.connectToSql();
+					String procedureCall = "{call InsertUserWithTable(?, ?, ?)}";
+		    		CallableStatement callableStatement = connection.prepareCall(procedureCall);
+		    		callableStatement.setString(1, name);
+		    		callableStatement.setString(2, email);
+		    		callableStatement.setString(3, hashedPassword);
+		    		callableStatement.execute();
+					connection.close();	
+				}
+			} catch (SQLException e) {
+				throw new IllegalStateException("Cannot connect the database!", e);
+			}
+			return success;
+	}
+	
+	/**
+	 * Checks the user is exist in the database
+	 * @param name : Username
+	 * @param email : Email
+	 * @return	Boolean value if true the user is exist
+	 */
+	private Boolean isExist(String name, String email) {
+			
+			Boolean exist = false;
+			try {
+				Connection connection = Func.connectToSql();
+				Statement statement = connection.createStatement();
+				ResultSet result = statement.executeQuery("SELECT * FROM login WHERE name = '"+name+"' || email='"+email+"' LIMIT 1;");
+				result.next();
+				if(result.getRow() == 1) {
+					exist = true;
+				}
+				statement.close();
+				connection.close();
+			} catch (SQLException e) {
+				throw new IllegalStateException("Cannot connect the database!", e);
+			}
+			return exist;
+		}
 	}
